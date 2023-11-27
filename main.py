e263cfd11152835c869734a11537a9c69cc1a5ce
Eden Valentinov <138798355+Edenval@users.noreply.github.com>
2023-11-26 23:31:46 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -14,20 +14,44 @@ statefile = pandas.read_csv("50_states.csv")
 statefile["x"] = statefile["x"].astype(int)
 statefile["y"] = statefile["y"].astype(int)
 
+newlist = statefile["state"].to_list()
+print(newlist)
 gameison = True
 guessstate = "Guess the state"
 popo = rules.Write()
 
+statelist = []
+
 while gameison:
     popo.Ask()
+    if popo.stet == "Exit":
+        popo.bye()
+        #if state not in statelist:
+         #   dictionar["State"] = state
+        break
+
     for state in statefile["state"]:
         if popo.stet == state:
-            print(state)
             xe = statefile[statefile["state"] == state]
             x = int(xe.x)
             ye = int(xe.y)
             popo.Go(x, ye, state)
             popo.Score()
+            statelist.append(state)
+            #state = state
+            print(state)
+            newlist.remove(state)
+            print(newlist)
+
+
+new_data = pandas.DataFrame(newlist)
+new_data.to_csv("states_to_learn.csv")
+
+# for state in statefile["state"]:
+#     if state not in statelist:
+#         diction["State"] = "state"
+
+#states_to_learn.csv
 
 
 turtle.mainloop()
